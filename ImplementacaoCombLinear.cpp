4c318d090a2341460497edf13e98fa80c167820b
diego <136401880+DKB-17@users.noreply.github.com>
2024-06-16 11:57:00 -0300

    feat: completo

--- ImplementacaoCombLinear.cpp
+++ ImplementacaoCombLinear.cpp
@@ -26,6 +26,18 @@ void imprimir_mat(float mat[][maxT], int ordem){
 
 float determinante_metodo_gauus(float mat[][maxT], int ordem){
 
+    cout << endl;
+    for(int i=0;i<ordem;i++){
+        for(int j=0;j<ordem;j++){
+            cout << mat[i][j] << "\t";
+            if(j<(ordem-1)){
+                cout << "|";
+            }
+        }
+        cout << endl;
+    }
+
+
     float submat[maxT][maxT];
     int i, j, k;
     float pivote1, pivote2;
@@ -50,7 +62,47 @@ float determinante_metodo_gauus(float mat[][maxT], int ordem){
         det *=submat[i][i];
     }
 
+    cout << endl;
+    for(int i=0;i<ordem;i++){
+        for(int j=0;j<ordem;j++){
+            cout << submat[i][j] << "\t";
+            if(j<(ordem-1)){
+                cout << "|";
+            }
+        }
+        if(i<(ordem-1)){
+            cout << endl;
+        }else{
+            cout << "\tDet: " << det;
+        }
+    }
+    cout << endl;
+
     return det;
+}
+
+void calculaCombinacaoLinear(float matI[][maxT], float vetR[], int ordem, float solucao[]){
+    float subMat[maxT][maxT];
+    for(int j=0;j<ordem;j++){
+        for(int k=0;k<ordem;k++){
+                subMat[k][j] = matI[j][k];
+        }
+    }
+    float detMI = determinante_metodo_gauus(subMat,ordem);
+    for(int i= 0; i<ordem;i++){
+        float mtAux[maxT][maxT];
+        for(int j=0;j<ordem;j++){
+            for(int k=0;k<ordem;k++){
+                    mtAux[k][j] = matI[j][k];
+            }
+        }
+        for(int j=0;j<ordem;j++){
+            mtAux[j][i] = vetR[j];
+        }
+        float detL = determinante_metodo_gauus(mtAux,ordem);
+
+        solucao[i] = detL/detMI ;
+    }
 }
 
 int main(){
@@ -59,14 +111,14 @@ int main(){
 
     int tamanho = 0;
 
-    do{
-    cout << "Digite o numero da quantidade de elementos da matiz: ";
-    cin >> tamanho;
-    }while(tamanho > maxT);
-
     float mat[maxT][maxT];
 
     do {
+
+        do{
+            cout << "Digite o numero da quantidade de elementos da matiz: ";
+            cin >> tamanho;
+        }while(tamanho > maxT);
 
         for(int i = 0; i<tamanho; i++) {
             cout << "\nLeitura do vetorV"<<(i+1);
@@ -79,22 +131,23 @@ int main(){
             float vetLI[tamanho];
             cout << "\nLeitura do vetor linear";
             lerVetor(vetLI,tamanho);
-            float cA[maxT][maxT];
+            float solucao[tamanho];
+            calculaCombinacaoLinear(mat, vetLI, tamanho, solucao);
+            cout << "\nSolucao: (";
             for(int i=0;i<tamanho;i++){
-                for(int j=0;j<tamanho;j++){
-                    cA[i][j] = mat[j][i];
+                cout << solucao[i];
+                if(i<(tamanho-1)){
+                    cout << "|";
                 }
             }
-            cout << "passei da troca";
-            imprimir_mat(cA, tamanho);
-
+            cout << ")";
         }else{
             cout << "\nDeterminante e igual a zero os vetores nao formam base, sendo LD :(";
         }
 
 
-        cout << "\nSair(0 = SIM): ";
+        cout << "\nSair(1 = SIM): ";
         cin >> op;
-    }while(op != 0);
+    }while(op != 1);
     return 0;
 }
